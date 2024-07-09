b7b16575160aa2a061de03a53797164f6eba5cb8
CK5115 <ckesharwani4@gmail.com>
2024-07-09 22:24:36 +0530

    started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
   return (
     <>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-      <Footer />
+      <div></div>
     </>
   );
 };
--- client/src/assets/assets.js
+++
@@ -1,7 +0,0 @@
-import logo from './logo.png'
-import logo_white from './logo_white.png'
-
-export const assets = {
-    logo,
-    logo_white,
-}
