d1f5e15e490388425f20ae4be246df2fe38d3523
CK5115 <ckesharwani4@gmail.com>
2024-07-13 21:25:39 +0530

    route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <div className="app overflow-hidden flex flex-col min-h-[100vh]">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
