9ae9d77bd23685b962848520cf1173625016bad3
CK5115 <ckesharwani4@gmail.com>
2024-09-07 11:34:21 +0530

    mongodb changed

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://chahat-webmark:Chahat1234@cluster0.qhrv3fd.mongodb.net/webmark').then(() => console.log('DB Connected'))
+  await mongoose.connect('mongodb+srv://chahat-webmark:Chahat1234@cluster0.oa5edgb.mongodb.net/webmark').then(() => console.log('DB Connected'))
 }
