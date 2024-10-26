dead9eaa90a665655b1a9b0e3a9e973fbd3294d1
CK5115 <ckesharwani4@gmail.com>
2024-10-26 17:00:11 +0530

    backend issue resolved

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js'
 import userRouter from './routes/userRoute.js'
-import bookmarkRouter from './routes/bookmarkRoutes.js'
+import router from './routes/bookmarkRoutes.js'
 import 'dotenv/config'
 
 
@@ -21,7 +21,7 @@ connectDB();
 
 // api endpoints
 app.use("/api/user", userRouter)
-app.use("/api/bookmarks", bookmarkRouter)
+app.use("/api/bookmarks", router)
 
 app.get('/', (req, res) => {
   res.send("API Working")
