9f5159a65881fcfafc1fd0b379c5f295bb01d532
CK5115 <ckesharwani4@gmail.com>
2024-10-26 16:52:56 +0530

    issue resolved

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://chahat-webmark:qwzOjkUcxoq5cbvj@cluster0.oa5edgb.mongodb.net/webmark').then(() => console.log('DB Connected'))
+  await mongoose.connect('mongodb+srv://webmark:aXKOFEyaPwQE8Tmv@cluster0.oa5edgb.mongodb.net/webmark').then(() => console.log('DB Connected'))
 }
